a99ed43684df749be9d9ce41ccff9171fe1df7b1
mehdi-saeeda <mehdisaeedaa@gmail.com>
2024-11-28 15:47:29 +0330

    Improve the code by adding some comments

--- presidential_election.py
+++ presidential_election.py
@@ -1,11 +1,13 @@
+# Main function to calculate the winning candidate number
 def main(n):
-    winner = 0
+    winner = 0    # Initialize winner index to 0 (starting point)
     for i in range(2, n + 1):
         winner = (winner + 2) % i
     return winner + 1
 
+# Function to get valid input from the user
 def get_input():
-    while True:
+    while True:     # Repeat until valid input is received
         try:
 
             n = input("Enter the number (2 ≤ n ≤ 100). ")
@@ -17,11 +19,12 @@ def get_input():
             if n < 2 or n > 100:
                 print("The number must be between 2 and 100.")
                 continue
-            return n
+            return n  # Return the valid input
 
         except ValueError:
             print("Invalid input Please enter a valid integer.")
 
+# Get the number from the user
 n = get_input()
 
 print(f"The winner is: {main(n)}")

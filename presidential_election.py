c4ac802770ecde589fe215dc319a0f8be6317c22
mehdi-saeeda <mehdisaeedaa@gmail.com>
2024-11-28 15:26:58 +0330

    Improve code using error handling

--- presidential_election.py
+++ presidential_election.py
@@ -1,8 +1,27 @@
 def main(n):
     winner = 0
-
     for i in range(2, n + 1):
         winner = (winner + 2) % i
     return winner + 1
-n = int(input("enter the number : "))
-print(main(n))
+
+def get_input():
+    while True:
+        try:
+
+            n = input("Enter the number (2 ≤ n ≤ 100). ")
+            if not n.isdigit():
+                print("Invalid input Please enter a valid integer.")
+                continue
+            n = int(n)
+
+            if n < 2 or n > 100:
+                print("The number must be between 2 and 100.")
+                continue
+            return n
+
+        except ValueError:
+            print("Invalid input Please enter a valid integer.")
+
+n = get_input()
+
+print(f"The winner is: {main(n)}")
